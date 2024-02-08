009f853707f42d06828244031ad0ae16564b43f7
Arthur Pradier <arthur.pradier81@gmail.com>
2024-02-08 20:56:16 +0100

    new mesh workflow and draw type
    deleted the VertexData struct, we now build our Mesh buffers via bufferSubData, storing each channel in a different Vector. This allows for a more convenient Mesh creation and edition. New draw to avoid passing through Model, useful when using Meshes generated at runtime (cf. terrain generation).

--- includes/sprout_engine/mesh.h
+++ includes/sprout_engine/mesh.h
@@ -10,21 +10,17 @@
 
 #include <assimp/aabb.h>
 
-struct VertexInfo
-{
-	glm::vec3 position;
-	glm::vec3 normal;
-	glm::vec2 uv;
-};
-
 class Mesh
 {
 protected:
-	std::vector<VertexInfo> vertices;
-	std::vector<unsigned int> indices;
-	std::vector<Texture> textures;
+	std::vector<glm::vec3> m_positions; 
+	std::vector<glm::vec3> m_normals;
+	std::vector<glm::vec2> m_texcoords;
+	std::vector<unsigned int> m_indices;
+	std::vector<Texture> m_textures;
 
 	AABB aabb;
+	//TODO move to the AABB class
 	unsigned int aabb_buff;
 	unsigned int aabb_vao;
 	Shader aabb_shader;
@@ -33,34 +29,32 @@ protected:
 	unsigned int index_buffer;
 	unsigned int vao;
 
+	bool update_data;
+
+protected:
+	inline size_t vertex_buffer_size() const { return m_positions.size() * sizeof(vec3); };
+	inline size_t normal_buffer_size() const { return m_normals.size() * sizeof(vec3); };
+	inline size_t texcoords_buffer_size() const { return m_texcoords.size() * sizeof(vec2); };
+	inline size_t indices_buffer_size() const { return m_indices.size() * sizeof(unsigned int); };
+
 public:
-	Mesh(const std::vector<VertexInfo> &vertices, const std::vector<unsigned int> &indices, const std::vector<Texture> &textures, const aiAABB &in_aabb)
-		: vertices(vertices), indices(indices), textures(textures)
+	Mesh() : update_data(false)
+	{
+		glGenVertexArrays(1, &vao);
+		glGenBuffers(1, &buffer);
+		glGenBuffers(1, &index_buffer);
+	};
+
+	// read a Mesh from file
+	Mesh(const std::vector<glm::vec3> &vertices, const std::vector<glm::vec3>& normals, const std::vector<glm::vec2>& texcoords, const std::vector<unsigned int> &indices, const std::vector<Texture> &textures, const aiAABB &in_aabb)
+		: update_data(false), m_positions(vertices), m_normals(normals), m_texcoords(texcoords), m_indices(indices), m_textures(textures)
 	{
 		// Mesh buffers/vao
 		glGenVertexArrays(1, &vao);
 		glGenBuffers(1, &buffer);
 		glGenBuffers(1, &index_buffer);
 
-		glBindVertexArray(vao);
-
-		// buffer containing all vertices data
-		glBindBuffer(GL_ARRAY_BUFFER, buffer);
-		glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(VertexInfo), vertices.data(), GL_STATIC_DRAW);
-
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer);
-		glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), indices.data(), GL_STATIC_DRAW);
-
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(VertexInfo), 0);
-		glEnableVertexAttribArray(0);
-
-		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(VertexInfo), (void*)offsetof(VertexInfo, normal));
-		glEnableVertexAttribArray(1);
-
-		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(VertexInfo), (void*)offsetof(VertexInfo, uv));
-		glEnableVertexAttribArray(2);
-
-		glBindVertexArray(0);
+		build_buffer();
 
 		//init AABB
 		aabb = AABB(glm::vec3(in_aabb.mMin.x, in_aabb.mMin.y, in_aabb.mMin.z), glm::vec3(in_aabb.mMax.x, in_aabb.mMax.y, in_aabb.mMax.z));
@@ -115,33 +109,141 @@ public:
 		glBindVertexArray(0);
 	};
 
-	void draw(Shader& s) const
+	//read-only accessors
+	const std::vector<glm::vec3>& positions() { return m_positions; };
+	const std::vector<glm::vec3>& normals() { return m_normals; };
+	const std::vector<glm::vec2>& texcoords() { return m_texcoords; };
+	const std::vector<unsigned int>& indices() { return m_indices; };
+
+	/**
+	 * \brief inserts a new vertex at the end of the array
+	 */
+	void vertex(glm::vec3 pos)
 	{
+		m_positions.push_back(pos);
+		update_data = true;
+	}
+
+	/**
+	 * \brief inserts a new normal at the end of the array
+	 */
+	void normal(glm::vec3 norm)
+	{
+		m_normals.push_back(norm);
+		update_data = true;
+	}
+
+	/**
+	 * \brief inserts a new texture coordinate at the end of the array
+	 */
+	void texcoord(glm::vec2 uv)
+	{
+		m_texcoords.push_back(uv);
+		update_data = true;
+	}
+
+	/**
+	 * \brief inserts the indices of a triangle in the structure using 3 vertex indices
+	 */
+	void triangle(int a, int b, int c)
+	{
+		m_indices.push_back(a);
+		m_indices.push_back(b);
+		m_indices.push_back(c);
+	}
+
+	/**
+	 * \brief Builds the buffer containing the mesh data. Since data will have to go from CPU to GPU,
+	 * it may be ineficient and will require a refactor, for instance using glBufferStorage...
+	 */
+	void build_buffer()
+	{
+		glBindVertexArray(vao);
+
+		// buffer containing all vertices data
+		glBindBuffer(GL_ARRAY_BUFFER, buffer);
+		size_t size = vertex_buffer_size() + normal_buffer_size() + m_texcoords.size() * sizeof(vec2);
+		glBufferData(GL_ARRAY_BUFFER, size, nullptr, GL_DYNAMIC_DRAW);
+
+		//positions subdata
+		size_t offset = 0;
+		size = vertex_buffer_size();
+		glBufferSubData(GL_ARRAY_BUFFER, offset, size, m_positions.data());
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)offset);
+		glEnableVertexAttribArray(0);
+
+		//normals 
+		offset += size;
+		size = normal_buffer_size();
+		glBufferSubData(GL_ARRAY_BUFFER, offset, size, m_normals.data());
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, (void*)offset);
+		glEnableVertexAttribArray(1);
+
+		//uvs
+		offset += size;
+		size = texcoords_buffer_size();
+		glBufferSubData(GL_ARRAY_BUFFER, offset, size, m_texcoords.data());
+		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 0, (void*)offset);
+		glEnableVertexAttribArray(2);
+
+		//index buffer
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices_buffer_size(), m_indices.data(), GL_DYNAMIC_DRAW);
+
+		glBindVertexArray(0);
+
+		update_data = false;
+	}
+
+	// all the different draws
+	void draw(Shader& s)
+	{
+		if (update_data)
+			build_buffer();
+
 		s.use();
 
 		// bind textures
 		unsigned int diffuse_id = 1;
 		unsigned int specular_id = 1;
-		for (int i = 0; i < textures.size(); i++)
+		for (int i = 0; i < m_textures.size(); i++)
 		{
 			glActiveTexture(GL_TEXTURE0 + i); //activate the correct texture unit
 			std::string number;
-			std::string name = textures[i].type;
+			std::string name = m_textures[i].type;
 			if (name == "texture_diffuse")
 				number = std::to_string(diffuse_id++);
 			if (name == "texture_specular")
 				number = std::to_string(specular_id++);
 			s.uniform_data((name + number).c_str(), i); // give the correct location to each texture
-			glBindTexture(GL_TEXTURE_2D, textures[i].get_id());
+			glBindTexture(GL_TEXTURE_2D, m_textures[i].get_id());
 		}
 		glActiveTexture(GL_TEXTURE0);
 
 		glBindVertexArray(vao);
-		glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, m_indices.size(), GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
 	};
 
-	void draw(Shader &s, const Frustum &frustum, const Transform& transform) const
+	/**
+	 * Draw a mesh at the center of the world
+	 * \param s a shader that will receive an mvp matrix and texture
+	 * \param c a camera to get a view and a projection
+	 * \param t a texture to put on the Mesh
+	 */		
+	void draw_flat(Shader& s)
+	{
+		if (update_data)
+			build_buffer();
+
+		s.use();
+
+		glBindVertexArray(vao);
+		glDrawElements(GL_TRIANGLES, m_indices.size(), GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
+	};
+
+	void draw(Shader &s, const Frustum &frustum, const Transform& transform)
 	{
 		//check if the aabb is visible (inside the frustum)
 		//convert aabb to center-extents
@@ -151,6 +253,7 @@ public:
 			draw(s);
 	};
 
+	//TODO: move to a draw specific to the AABB class...
 	void drawAABB(Shader& s, const glm::mat3& transform, const glm::vec3& translation)
 	{
 		s.use();
@@ -177,7 +280,7 @@ public:
 		}
 
 		glBindVertexArray(vao);
-		glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, m_indices.size(), GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
 	};
 };
