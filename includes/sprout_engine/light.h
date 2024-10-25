54de95e1926eebce807da663e9d57b2257683fdd
Arthur Pradier <arthur.pradier81@gmail.com>
2024-10-25 08:58:13 +0200

    feature(Lights): Lights now use Color (aka vec4)
    Allows for a better alignment when sending data to a shader

--- includes/sprout_engine/light.h
+++ includes/sprout_engine/light.h
@@ -4,79 +4,86 @@
 #include <glm/glm.hpp>
 
 #include<sprout_engine/shader.h>
+#include<sprout_engine/color.h>
 
 #include <string>
 
 using namespace glm;
 
-class Light
+class Light //: public Inspectable
 {
 protected:
-    vec3 ambiant;
-    vec3 diffuse;
-    vec3 specular;
+    Color ambiant;
+    Color diffuse;
+    Color specular;
 
 public:
-    Light() : ambiant(vec3(.1f, .1f, .1f)), diffuse(vec3(1., 1., 1.)), specular(vec3(.9, .9, .9)) {};
-    Light(vec3 amb, vec3 diff, vec3 spec) : ambiant(amb), diffuse(diff), specular(spec) {};
+    Light() : ambiant(.1f, .1f, .1f, 1.f), diffuse(1.f, 1.f, 1.f, 1.f), specular(.9, .9, .9, 1.f) {};
+    Light(Color amb, Color diff, Color spec) : ambiant(amb), diffuse(diff), specular(spec) {};
 
     void set_colors(float* amb, float* diff, float* spec)
     { 
-        ambiant = glm::vec3(amb[0], amb[1], amb[2]); 
-        diffuse = glm::vec3(diff[0], diff[1], diff[2]);
-        specular = glm::vec3(spec[0], spec[1], spec[2]);
+        ambiant = glm::vec4(amb[0], amb[1], amb[2], 1.f);
+        diffuse = glm::vec4(diff[0], diff[1], diff[2], 1.f);
+        specular = glm::vec4(spec[0], spec[1], spec[2], 1.f);
     };
 
     virtual void send_to_shader(Shader &s, int index) = 0;
+    //virtual void drawInspector() = 0;
 };
 
 class DirectionalLight : public Light
 {
 protected:
-    vec3 direction;
+    vec4 direction;
 
 public:
-    DirectionalLight() : Light(), direction(vec3(0., -1., 0.)) {};
-    DirectionalLight(glm::vec3 direction, glm::vec3 ambiant, glm::vec3 diffuse, glm::vec3 specular)
+    DirectionalLight() : Light(), direction(0., -1., 0., 1.) {};
+    DirectionalLight(glm::vec4 direction, Color ambiant, Color diffuse, Color specular)
         : Light(ambiant, diffuse, specular), direction(direction) {};
 
-    [[nodiscard]] glm::vec3 getDirection() const
+    [[nodiscard]] glm::vec4 getDirection() const
     {
         return direction;
     };
 
-    void set_direction(float* dir) { direction = vec3(dir[0], dir[1], dir[2]); };
+    void set_direction(float* dir) { direction = vec4(dir[0], dir[1], dir[2], 1.f); };
 
-    void send_to_shader(Shader& s, int index)
+    void send_to_shader(Shader& s, int index) override
     {
         s.use();
-        s.uniform_data(std::string("dirLights[").append(std::to_string(index)).append("].direction"), direction[0], direction[1], direction[2]);
-        s.uniform_data(std::string("dirLights[").append(std::to_string(index)).append("].ambiant"), ambiant[0], ambiant[1], ambiant[2]);
-        s.uniform_data(std::string("dirLights[").append(std::to_string(index)).append("].diffuse"), diffuse[0], diffuse[1], diffuse[2]);
-        s.uniform_data(std::string("dirLights[").append(std::to_string(index)).append("].specular"), specular[0], specular[1], specular[2]);
+        s.uniform_data(std::string("dirLights[").append(std::to_string(index)).append("].direction"), direction[0], direction[1], direction[2], direction[3]);
+        s.uniform_data(std::string("dirLights[").append(std::to_string(index)).append("].ambiant"), ambiant[0], ambiant[1], ambiant[2], ambiant[3]);
+        s.uniform_data(std::string("dirLights[").append(std::to_string(index)).append("].diffuse"), diffuse[0], diffuse[1], diffuse[2], diffuse[3]);
+        s.uniform_data(std::string("dirLights[").append(std::to_string(index)).append("].specular"), specular[0], specular[1], specular[2], specular[3]);
     };
+
+    // void drawInspector() override
+    // {
+    //
+    // }
 };
 
-struct PointLight : public Light
+class PointLight : public Light
 {
 protected:
-    vec3 position;
+    vec4 position;
     float constant;
     float linear;
     float quadratic;
 
 public:
-    PointLight() : Light(), position(vec3(0., 0., 0.)), constant(1.f), linear(.22f), quadratic(.2f) {};
-    PointLight(vec3 position, vec3 ambiant, vec3 diffuse, vec3 specular, float constant = 1.f, float linear = .22f, float quadratic = .2f)
+    PointLight() : Light(), position(0., 0., 0., 1.), constant(1.f), linear(.22f), quadratic(.2f) {};
+    PointLight(vec4 position, vec4 ambiant, vec4 diffuse, vec4 specular, float constant = 1.f, float linear = .22f, float quadratic = .2f)
         : Light(ambiant, diffuse, specular),  position(position), constant(constant), linear(linear), quadratic(quadratic) {};
     
-    void send_to_shader(Shader& s, int index)
+    void send_to_shader(Shader& s, int index) override
     {
         s.use();
-        s.uniform_data(std::string("pointLights[").append(std::to_string(index)).append("].position"), position[0], position[1], position[2]);
-        s.uniform_data(std::string("pointLights[").append(std::to_string(index)).append("].ambiant"), ambiant[0], ambiant[1], ambiant[2]);
-        s.uniform_data(std::string("pointLights[").append(std::to_string(index)).append("].diffuse"), diffuse[0], diffuse[1], diffuse[2]);
-        s.uniform_data(std::string("pointLights[").append(std::to_string(index)).append("].specular"), specular[0], specular[1], specular[2]);
+        s.uniform_data(std::string("pointLights[").append(std::to_string(index)).append("].position"), position[0], position[1], position[2], position[3]);
+        s.uniform_data(std::string("pointLights[").append(std::to_string(index)).append("].ambiant"), ambiant[0], ambiant[1], ambiant[2], ambiant[3]);
+        s.uniform_data(std::string("pointLights[").append(std::to_string(index)).append("].diffuse"), diffuse[0], diffuse[1], diffuse[2], diffuse[3]);
+        s.uniform_data(std::string("pointLights[").append(std::to_string(index)).append("].specular"), specular[0], specular[1], specular[2], specular[3]);
 
         s.uniform_data(std::string("pointLights[").append(std::to_string(index)).append("].constant"), constant);
         s.uniform_data(std::string("pointLights[").append(std::to_string(index)).append("].linear"), linear);
